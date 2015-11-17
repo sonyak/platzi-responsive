f43d1430da5c065661098a08fae21091dde2af5c
Sonia <sony08pat@hotmail.com>
2015-11-17 17:26:14 -0500

    gulpfile

--- proyecto/gulpfile.js
+++ proyecto/gulpfile.js
@@ -1,12 +1,19 @@
 /**
  * Created by Sonia on 15/11/15.
  */
-var gulp= require('gulp');
-var webserver = require('gulp-webserver');
-var stylus = require('gulp-stylus');
-var nib= require('nib');
-var minifyCSS= require('gulp-minify-css');
-var config={
+var gulp        = require('gulp');
+var webserver   = require('gulp-webserver');
+var stylus      = require('gulp-stylus');
+var nib         = require('nib');
+var minifyCSS   = require('gulp-minify-css');
+var browserify  = require('browserify');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
+var uglify      = require('gulp-uglify');
+var smoosher    = require('gulp-smoosher');
+var imageop     = require('gulp-image-optimization');
+
+var config      = {
     styles:{
         main:'./src/styles/main.styl',
         watch:'./src/styles/**/*.styl',
@@ -14,6 +21,15 @@ var config={
     },
     html:{
         watch:'./build/*.html'
+    },
+    scripts:{
+        main:'./src/scripts/main.js',
+        watch:'./src/scripts/**/*.js',
+        output:'./build/js'
+    },
+    images:{
+        watch: ['./build/img/*.png','./build/img/*.jpg'],
+        output:'./dist/img'
     }
 }
 gulp.task('server',function(){
@@ -33,9 +49,34 @@ gulp.task('build:css',function(){
         .pipe(minifyCSS())
         .pipe(gulp.dest(config.styles.output));
 });
+
+gulp.task('build:js',function(){
+    return browserify(config.scripts.main)
+    .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest(config.scripts.output))
+});
 gulp.task('watch',function(){
+    gulp.watch(config.images.watch,['images']);
+    gulp.watch(config.scripts.watch,['build:js']);
     gulp.watch(config.styles.watch,['build:css']);
     gulp.watch(config.html.watch,['build']);
 });
-gulp.task('build',['build:css']);
+gulp.task('images',function(){
+    gulp.src(config.images.watch)
+        .pipe(imageop({
+            optimizationLevel:5,
+            progressive: true,
+            interlaced:true
+        }))
+        .pipe(gulp.dest(config.images.output));
+});
+gulp.task('inline',function(){
+    gulp.src('./build/index.html')
+        .pipe(smoosher())
+        .pipe(gulp.dest('./dist'));
+});
+gulp.task('build',['build:css','build:js','images','inline']);
 gulp.task('default',['server','watch','build']);
